c770efe57a9ec1e560820552d2fe98740c031d79
sammanomar <sammanomar@hotmail.com>
2023-04-01 22:38:50 +0000

    fix pictures and design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,94 @@
-window.addEventListener('load',shuffleAll);
-let allLevelDivs = document.querySelectorAll('[class*="level"]');
-let twoSlices = [];
-let currentView = 'level-1';
+var timerFunction;
 
-function shuffleAll(){
-    for (const levelDiv of allLevelDivs) {
-        let allSlices = levelDiv.querySelectorAll('[class*=img-holder]');
-        let orderArray = [];
-        for (let i = 0; i < allSlices.length; i++) {
-            orderArray.push(i+1)
+var imagePuzzle = {
+    stepCount: 0,
+    startTime: new Date().getTime(),
+    startGame: function (images, gridSize) {
+        this.setImage(images, gridSize);
+        helper.doc('playPanel').style.display = 'block';
+        helper.shuffle('sortable');
+        this.stepCount = 0;
+        this.startTime = new Date().getTime();
+        this.tick();
+    },
+    tick: function () {
+        var now = new Date().getTime();
+        var elapsedTime = parseInt((now - imagePuzzle.startTime) / 1000, 10);
+        helper.doc('timerPanel').textContent = elapsedTime;
+        timerFunction = setTimeout(imagePuzzle.tick, 1000);
+    },
+    setImage: function (images, gridSize = 4) {
+        var percentage = 100 / (gridSize - 1);
+        var image = images[Math.floor(Math.random() * images.length)];
+        helper.doc('imgTitle').innerHTML = image.title;
+        helper.doc('actualImage').setAttribute('src', image.src);
+        helper.doc('sortable').innerHTML = '';
+        for (var i = 0; i < gridSize * gridSize; i++) {
+            var xpos = (percentage * (i % gridSize)) + '%';
+            var ypos = (percentage * Math.floor(i / gridSize)) + '%';
+
+            let li = document.createElement('li');
+            li.id = i;
+            li.setAttribute('data-value', i);
+            li.style.backgroundImage = 'url(' + image.src + ')';
+            li.style.backgroundSize = (gridSize * 100) + '%';
+            li.style.backgroundPosition = xpos + ' ' + ypos;
+            li.style.width = 400 / gridSize + 'px';
+            li.style.height = 400 / gridSize + 'px';
+
+            li.setAttribute('draggable', 'true');
+            li.ondragstart = (event) => event.dataTransfer.setData('data', event.target.id);
+            li.ondragover = (event) => event.preventDefault();
+            li.ondrop = (event) => {
+                let origin = helper.doc(event.dataTransfer.getData('data'));
+                let dest = helper.doc(event.target.id);
+                let p = dest.parentNode;
+
+                if (origin && dest && p) {
+                    let temp = dest.nextSibling;
+                    let x_diff = origin.offsetLeft-dest.offsetLeft;
+                    let y_diff = origin.offsetTop-dest.offsetTop;
+
+                    if(y_diff == 0 && x_diff >0){
+                        //LEFT SWAP
+                        p.insertBefore(origin, dest);
+                        p.insertBefore(temp, origin);
+                    }
+                    else{
+                        p.insertBefore(dest, origin);
+                        p.insertBefore(origin, temp);
+                    }
+
+
+                    let vals = Array.from(helper.doc('sortable').children).map(x => x.id);
+                    var now = new Date().getTime();
+                    helper.doc('stepCount').textContent = ++imagePuzzle.stepCount;
+                    document.querySelector('.timeCount').textContent = (parseInt((now - imagePuzzle.startTime) / 1000, 10));
+
+                    if (isSorted(vals)) {
+                        // helper.doc('actualImageBox').style.display = 'none';
+                        // helper.doc('gameOver').style.display = 'block';
+                        helper.doc('actualImageBox').innerHTML = helper.doc('gameOver').innerHTML;
+                        helper.doc('stepCount').textContent = imagePuzzle.stepCount;
+                    }
+                }
+            };
+            li.setAttribute('dragstart', 'true');
+            helper.doc('sortable').appendChild(li);
         }
-        for (let i = 0; i < allSlices.length; i++) {
-           let rand = Math.floor(Math.random()*orderArray.length)
-            allSlices[i].style.order = orderArray[rand];
-            orderArray.splice(rand,1);
+        helper.shuffle('sortable');
+    }
+};
+
+isSorted = (arr) => arr.every((elem, index) => { return elem == index; });
+
+var helper = {
+    doc: (id) => document.getElementById(id) || document.createElement("div"),
+
+    shuffle: (id) => {
+        var ul = document.getElementById(id);
+        for (var i = ul.children.length; i >= 0; i--) {
+            ul.appendChild(ul.children[Math.random() * i | 0]);
         }
-    }
-    startGame();
-}
-
-function startGame(){
-    // set links to show hide
-    setNavLinks();
-    // set clicks on all slices
-    addClicksToSlices();
-}
-
-function addClicksToSlices(){
-    let allSlices = document.querySelectorAll('[class*="img-holder"]');
-    for (const slice of allSlices) {
-        slice.addEventListener('click',selectMe)
-    }
-}
-function selectMe(){
-    this.style.border = "2px solid green";
-    twoSlices.push(this);
-    if(twoSlices.length === 2){
-        // get order of clicks
-        let orderFirst = window.getComputedStyle(twoSlices[0]).getPropertyValue('order');
-        let orderSecond = window.getComputedStyle(twoSlices[1]).getPropertyValue('order');
-        //reoreder
-        twoSlices[0].style.order = orderSecond;
-        twoSlices[1].style.order = orderFirst;
-
-        twoSlices[0].style.border = "none";
-        twoSlices[1].style.border = "none";
-
-        // reset 
-        twoSlices.length = 0;
-        checkIsComplete()
-    }
-}
-
-function checkIsComplete(){
-    let currentDiv = document.querySelector('.'+currentView);
-    let allSlices = currentDiv.querySelectorAll('[class*="img-holder"]');
-    let correctOrder = [];
-    for (let i = 0; i < allSlices.length; i++) {
-        correctOrder.push(i+1)
-    }
-    let currentOrder = [];
-    for (let i = 0; i < allSlices.length; i++) {
-        const slice = allSlices[i];
-        currentOrder.push(window.getComputedStyle(slice).getPropertyValue('order'))
-    }
-    if(currentOrder.toString() == correctOrder.toString()){
-        let activeLink = document.querySelector('.active');
-        activeLink.classList.add('finished');
-        currentDiv.style.border = "5px solid green";
-        currentDiv.style.boxShadow = "0 0 16px green";
-    }
-
-}
-
-
-function setNavLinks(){
-    let headerNavLinks = document.querySelectorAll('[data-lvl]');
-
-    for (let i = 0; i < headerNavLinks.length; i++) {
-        const link = headerNavLinks[i];
-        link.addEventListener('click',function(){
-             currentView = this.getAttribute('data-lvl');
-             for (const mylink of headerNavLinks) {
-                 mylink.classList.remove('active');
-             }
-             this.classList.add('active');
-            for (let i = 0; i < allLevelDivs.length; i++) {
-                allLevelDivs[i].style.display = "none";
-            }
-            let divToShow = document.querySelector('.'+currentView);
-            divToShow.style.display = "flex";
-        })
     }
 }
